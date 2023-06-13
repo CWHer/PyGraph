d94ebe4e49c051da7a719fd6cf0566264496a070
CWHer <cwher@outlook.com>
2023-06-14 00:09:00 +0800

    feat: use f32 type in edge weight

--- cgraph/graph.hpp
+++ cgraph/graph.hpp
@@ -13,14 +13,15 @@ class Graph
 {
 protected:
     using i32 = int;
-    using u32 = unsigned int;
-    using i64 = long long;
+    using f32 = float;
+    using f64 = double;
 
     i32 num_nodes, num_edges;
 
     struct Edge
     {
-        i32 from, to, weight;
+        i32 from, to;
+        f32 weight;
     };
     std::vector<std::vector<Edge>> edges_per_node;
 
@@ -52,7 +53,7 @@ public:
         return std::make_tuple(num_nodes, num_edges);
     }
 
-    void addEdge(i32 from, i32 to, i32 weight)
+    void addEdge(i32 from, i32 to, f32 weight)
     {
         assert(!empty());
         assert(0 <= from && from < num_nodes);
@@ -63,18 +64,17 @@ public:
     }
 
     // Graph Ops
-    std::vector<i64> getAllShortestPath()
+    std::vector<f64> getAllShortestPath()
     {
 #ifdef _OPENMP
         std::cout << "[INFO]: OMP enabled, "
-                  << "OMP_NUM_THREADS=" << std::getenv("OMP_NUM_THREADS")
+                  << "OMP_NUM_THREADS=" << std::getenv("OMP_NUM_THREADS") << '\n'
                   << std::endl;
 #else
         std::cout << "[WARN]: OMP not enabled" << std::endl;
 #endif
         // Use Floyd Algorithm
-        std::vector<i64> dist(
-            num_nodes * num_nodes, std::numeric_limits<i64>::max());
+        std::vector<f64> dist(num_nodes * num_nodes, std::numeric_limits<f64>::max());
         // clang-format off
         auto arrIdx = [&](i32 i, i32 j){ return i * num_nodes + j; };
         // clang-format on
@@ -82,32 +82,32 @@ public:
         {
             dist[arrIdx(i, i)] = 0;
             for (const auto &e : edges_per_node[i])
-                dist[arrIdx(i, e.to)] = std::min(dist[arrIdx(i, e.to)], (i64)e.weight);
+                dist[arrIdx(i, e.to)] = std::min(dist[arrIdx(i, e.to)], (f64)e.weight);
         }
         for (i32 k = 0; k < num_nodes; k++)
 #pragma omp parallel for schedule(dynamic)
             for (i32 i = 0; i < num_nodes; i++)
-                for (i32 j = 0; j < num_nodes; j++)
-                    if (dist[arrIdx(i, k)] < std::numeric_limits<i64>::max() &&
-                        dist[arrIdx(k, j)] < std::numeric_limits<i64>::max())
-                    {
-                        dist[arrIdx(i, j)] = std::min(dist[arrIdx(i, j)],
-                                                      dist[arrIdx(i, k)] + dist[arrIdx(k, j)]);
-                    }
+                if (dist[arrIdx(i, k)] != std::numeric_limits<f64>::max())
+                    for (i32 j = 0; j < num_nodes; j++)
+                        if (dist[arrIdx(k, j)] != std::numeric_limits<f64>::max())
+                        {
+                            dist[arrIdx(i, j)] = std::min(dist[arrIdx(i, j)],
+                                                          dist[arrIdx(i, k)] + dist[arrIdx(k, j)]);
+                        }
 
         return dist;
     }
 
-    std::vector<i64> getShortestPathCond(i32 src)
+    std::vector<f64> getShortestPathCond(i32 src)
     {
         assert(!empty());
         assert(0 <= src && src < num_nodes);
 
         // Use Dijsktra's Algorithm
-        std::vector<i64> dist(
-            num_nodes, std::numeric_limits<i64>::max());
+        std::vector<f64> dist(
+            num_nodes, std::numeric_limits<f64>::max());
         std::vector<bool> visited(num_nodes, false);
-        std::priority_queue<std::pair<i64, i32>> pq;
+        std::priority_queue<std::pair<f64, i32>> pq;
 
         dist[src] = 0;
         pq.push(std::make_pair(0, src));
@@ -133,14 +133,14 @@ public:
         return dist;
     }
 
-    std::vector<i64> getShortestPath(i32 src)
+    std::vector<f64> getShortestPath(i32 src)
     {
         assert(!empty());
         assert(0 <= src && src < num_nodes);
 
         // Use SPFA Algorithm
-        std::vector<i64> dist(
-            num_nodes, std::numeric_limits<i64>::max());
+        std::vector<f64> dist(
+            num_nodes, std::numeric_limits<f64>::max());
         std::vector<bool> in_queue(num_nodes, false);
         std::queue<i32> q;
 
